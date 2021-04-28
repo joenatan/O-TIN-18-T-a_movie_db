ceba761cea13fc05ed34d4b8887a64d41f06e236
jonathan <j.brutschin@gmail.com>
2021-04-28 17:21:08 +0200

    pimpup person movie

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Movie, Genre
+from .models import Movie, Genre, Person, PersonMovie
 
 # https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
 
@@ -8,14 +8,36 @@ from .models import Movie, Genre
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     search_fields = ['title', 'description']
-    list_display = ['__str__', 'fsk', 'get_genres']
+    list_display = ['__str__', 'fsk', 'get_genres', 
+        'get_directors', 'get_actors']
     list_filter = ['genres', 'fsk']
 
     def get_genres(self, obj):
         return obj.genres_printable_list
     get_genres.short_description = 'Genres'
 
+    def get_directors(self, obj):
+        return obj.people_printable_list('director')
+    get_directors.short_description = 'Directors'
+
+    def get_actors(self, obj):
+        return obj.people_printable_list('actor')
+    get_actors.short_description = 'Actors'
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'count_movies']
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
+
+
+@admin.register(PersonMovie)
+class PersonMovieAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'get_movie', 'type']
+    list_filter = ['type']
+
+    def get_movie(self, obj):
+        return obj.print_movie
--- backend/models.py
+++ backend/models.py
@@ -20,10 +20,14 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
         
-    
     @property
     def genres_printable_list(self):
         return list(self.genres.select_related())
+        
+    def people_printable_list(self, person_type):
+        # https://docs.djangoproject.com/en/3.2/ref/models/querysets
+        people = self.person_movies.select_related().filter(type=person_type)
+        return list(people)
 
 
 class Genre(models.Model):
@@ -38,3 +42,41 @@ class Genre(models.Model):
         Return count movies of current instance
         """
         return self.movies.select_related().count()
+
+
+class Person(models.Model):
+    last_name = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256)
+    age = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
+
+    def __str__(self):
+        return '%s %s (%i)' % (self.first_name, self.last_name, self.age)
+
+
+# https://docs.djangoproject.com/en/3.2/ref/models/fields/#choices
+
+PERSON_TYPE_CHOICES = (
+    ('actor', 'Actor'),
+    ('director', 'Director')
+)
+
+class PersonMovie(models.Model):
+    type = models.CharField(max_length=30, choices=PERSON_TYPE_CHOICES, default='actor')
+
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE, related_name='person_movies')
+    person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name='person_movies')
+
+    def __str__(self):
+        return '%s %s' % (self.person.first_name, self.person.last_name)
+
+    @property
+    def full_name(self):
+        return '%s %s' % (self.person.first_name, self.person.last_name)
+
+    @property
+    def print_movie(self):
+        return self.movie.title
