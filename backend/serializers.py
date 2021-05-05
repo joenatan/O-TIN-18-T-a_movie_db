7518b242bd25a36230341848a90a4689ee61c683
jonathan <j.brutschin@gmail.com>
2021-05-05 16:20:55 +0200

    nested serializer on movies

--- backend/serializers.py
+++ backend/serializers.py
@@ -3,11 +3,34 @@ from rest_framework import serializers
 from .models import Movie, Genre
 
 
+class GenreNestedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ['id', 'name']
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreNestedSerializer(many=True)
     
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'fsk']
+
+        # Variant 1
+        #fields = ['id', 'title', 'description', 'fsk', 'duration', 'genres']
+
+        # Variate 2 (Exclude)
+        #exclude = ['genres']
+
+        # Variante 3 alles darstellen
+        fields = '__all__'
+
+
+class MovieCreateSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 class GenreSerializer(serializers.ModelSerializer):
--- backend/views.py
+++ backend/views.py
@@ -2,12 +2,17 @@ from rest_framework import viewsets
 
 
 from .models import Movie, Genre
-from .serializers import MovieSerializer, GenreSerializer
+from .serializers import MovieSerializer, MovieCreateSerializer, GenreSerializer
 
 
 class MovieApiViewSet(viewsets.ModelViewSet):
-    serializer_class = MovieSerializer
+    # serializer_class = MovieSerializer
     queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create' or self.action == 'update':
+            return MovieCreateSerializer
+        return MovieSerializer
 
 
 class GenreApiViewSet(viewsets.ModelViewSet):
