4d450582f2d76644d94943e3088da459213bb303
jonathan <j.brutschin@gmail.com>
2021-05-12 15:15:17 +0200

    add custom description in drf-yasg

--- backend/views.py
+++ backend/views.py
@@ -1,12 +1,18 @@
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django.utils.decorators import method_decorator
 
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import Movie, Genre
 from .serializers import MovieSerializer, MovieCreateSerializer, GenreSerializer
 
-
+# Custom description
+# https://drf-yasg.readthedocs.io/en/stable/custom_spec.html
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="get a list of movies"
+))
 class MovieApiViewSet(viewsets.ModelViewSet):
     # serializer_class = MovieSerializer
     queryset = Movie.objects.all()
