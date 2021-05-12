347f3467fc66f7400111df747fa2161580c6a284
jonathan <j.brutschin@gmail.com>
2021-05-12 16:39:41 +0200

    install jwt auth

--- movie_db/urls.py
+++ movie_db/urls.py
@@ -5,6 +5,11 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Movie API",
@@ -18,8 +23,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api/v1/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
-    #path('api/v1/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/v1/', include('backend.urls')),
-    path('admin/', admin.site.urls),
+   path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   path('api/v1/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
+   #path('api/v1/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('api/v1/', include('backend.urls')),
+   path('admin/', admin.site.urls),
 ]
